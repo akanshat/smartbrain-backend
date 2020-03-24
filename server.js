2497e0a91fe5adbd243e04e6ab0c99105a0a7555
Akansha Tiwari <akanshat1999@gmail.com>
2020-03-25 01:34:53 +0530

    finalised

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image =  require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : process.env.DATABASE_URL,
       user : 'test',
       password : 'test123',
       database : 'smart_brain'
